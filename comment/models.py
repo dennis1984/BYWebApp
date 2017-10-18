b9e30a73e3efe39b00efcf355f9b50b1d234365d
dennis1984 <year2005706@163.com>
2017-10-18 18:21:39 +0800

    评论及媒体资源models完善

--- comment/models.py
+++ comment/models.py
@@ -15,33 +15,17 @@ class Comment(models.Model):
     用户点评
     """
     user_id = models.IntegerField('用户ID')
-    orders_id = models.CharField('订单ID', db_index=True, unique=True, max_length=32)
-    business_id = models.IntegerField('商户ID')
-    business_name = models.CharField('商户名称', max_length=100)
-    business_comment = models.TextField('商户的点评内容')
-    # 商户的点评内容的数据格式为：
-    # [{'id': 1,
-    #   'star': 3,
-    #   'cn_name': u'服务质量'},...
-    # ]
+    media_id = models.IntegerField('媒体资源ID', db_index=True)
+    content = models.CharField('点评内容', max_length=512)
 
-    dishes_comment = models.TextField('菜品的点评内容')
-    # 菜品的点评内容的数据格式为：
-    # [{'dishes_id':  1,
-    #    'dishes_name': '菜品名称',
-    #    'image': '/var/www/static/...',
-    #    'image_detail': '/var/www/static/...',
-    #    'star': 3}, ....
-    # ]
-
-    messaged = models.TextField('评价留言', null=True, blank=True)
+    reply_id = models.IntegerField('管理员回复评论ID', null=True)
     created = models.DateTimeField('创建时间', default=now)
 
     class Meta:
         db_table = 'by_comment'
 
     def __unicode__(self):
-        return self.orders_id
+        return '%s:%s' % (self.user_id, self.media_id)
 
     @classmethod
     def get_object(cls, **kwargs):
@@ -57,45 +41,6 @@ class Comment(models.Model):
         except Exception as e:
             return e
 
-    @classmethod
-    def filter_comment_details(cls, **kwargs):
-        instances = cls.filter_objects(**kwargs)
-        if isinstance(instances, Exception):
-            return instances
-        details = []
-        for instance in instances:
-            ins_dict = model_to_dict(instance)
-            ins_dict['business_comment'] = json.loads(ins_dict['business_comment'])
-            ins_dict['dishes_comment'] = cls.get_perfect_dishes_comment(ins_dict['dishes_comment'])
-            details.append(ins_dict)
-        return details
-
-    @classmethod
-    def get_comment_detail(cls, **kwargs):
-        instance = cls.get_object(**kwargs)
-        if isinstance(instance, Exception):
-            return instance
-        detail = model_to_dict(instance)
-        detail['business_comment'] = json.loads(detail['business_comment'])
-        detail['dishes_comment'] = cls.get_perfect_dishes_comment(detail['dishes_comment'])
-        return detail
-
-    @classmethod
-    def get_perfect_dishes_comment(cls, dishes_comment):
-        if isinstance(dishes_comment, (str, unicode)):
-            try:
-                dishes_comment = json.loads(dishes_comment)
-            except Exception as e:
-                return e
-        elif not isinstance(dishes_comment, (list, tuple)):
-            return Exception('Params data is error.')
-
-        details = []
-        # for dishes_detail in dishes_comment:
-        #     perfect_detail = get_perfect_detail_by_detail(Dishes, dishes_detail)
-        #     details.append(perfect_detail)
-        return details
-
 
 class ReplyComment(models.Model):
     """
@@ -103,7 +48,6 @@ class ReplyComment(models.Model):
     """
     comment_id = models.IntegerField(u'被回复点评的记录ID', unique=True, db_index=True)
     user_id = models.IntegerField('管理员用户ID')
-    orders_id = models.CharField('订单ID', max_length=32)
 
     messaged = models.TextField('评价留言', null=True, blank=True)
     created = models.DateTimeField('创建时间', default=now)
@@ -112,7 +56,7 @@ class ReplyComment(models.Model):
         db_table = 'by_reply_comment'
 
     def __unicode__(self):
-        return self.orders_id
+        return str(self.comment_id)
 
     @classmethod
     def get_object(cls, **kwargs):
--- media/models.py
+++ media/models.py
@@ -25,7 +25,7 @@ class Media(models.Model):
     """
     title = models.CharField('资源标题', max_length=32, db_index=True)
     subtitle = models.CharField('资源副标题', max_length=128, null=True, blank=True)
-    description = models.CharField('资源描述', max_length=256, null=True, blank=True)
+    description = models.TextField('资源描述/介绍', null=True, blank=True)
 
     # 资源类型：10：电影 20：电视剧 30：综艺节目
     media_type = models.IntegerField('资源类型', default=10)
@@ -34,10 +34,26 @@ class Media(models.Model):
     # 项目进度  1：筹备期 2：策划期 3：xxx
     progress = models.IntegerField('项目进度', default=1)
 
+    # 资源热度
+    temperature = models.FloatField('热度')
+    # 票房预测
+    box_office_forecast = models.FloatField('票房预测')
+    # 口碑预测
+    public_praise_forecast = models.FloatField('口碑预测')
+
+    # 资源属性：数据格式为JSON字符串，如：[1, 3, 5] （数字为属性ID）
+    attributes = models.CharField('资源属性', max_length=256, )
+
     # 导演：数据格式为JSON字符串，如：['斯皮尔伯格', '冯小刚']
     director = models.CharField('导演', max_length=256)
     # 主演：数据格式为JSON字符串，如：['汤姆克鲁斯', '威尔史密斯', '皮尔斯布鲁斯南']
     stars = models.CharField('主演', max_length=256)
+    # 演员：数据格式为JSON字符串，如：['王晓霞', '詹姆斯', '韦德']
+    actors = models.CharField('演员', max_length=256)
+    # 监制：数据格式为JSON字符串，如：['欧文']
+    producer = models.CharField('监制', max_length=256)
+    # 出品公司：数据格式为JSON字符串，如：['华文映像', '福星传媒']
+    production_company = models.CharField('出品公司', )
 
     recorded_time = models.DateTimeField('开机时间')
     air_time = models.DateTimeField('播出时间')
