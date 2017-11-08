09ef666ce566e09af8f5a55e67086abaeb802b46
dennis1984 <year2005706@163.com>
2017-11-08 16:13:26 +0800

    媒体资源model完善

--- media/models.py
+++ media/models.py
@@ -215,7 +215,7 @@ class ThemeType(models.Model):
     class Meta:
         db_table = 'by_theme_type'
         unique_together = ['name', 'media_type_id', 'status']
-        ordering = ['media_type_id', '-update']
+        ordering = ['media_type_id', '-updated']
 
     def __unicode__(self):
         return '%s' % self.name
