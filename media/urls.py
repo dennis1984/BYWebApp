13a04985bb4c1067a3582ae09a6921adc404bf2b
dennis1984 <year2005706@163.com>
2017-11-27 18:09:16 +0800

    资源相关：url配置

--- media/urls.py
+++ media/urls.py
@@ -4,8 +4,11 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from media import views
 
 urlpatterns = [
-    url(r'^collect_action/$', views.CollectAction.as_view()),
-    url(r'^resource_tags_list/$', views.ResourceTagsList.as_view()),
+    url(r'^media_type_list/$', views.MediaTypeList.as_view()),
+    url(r'^theme_type_list/$', views.ThemeTypeList.as_view()),
+    url(r'^progress_list/$', views.ProgressList.as_view()),
+    url(r'^media_detail/$', views.MediaDetail.as_view()),
+    url(r'^media_list/$', views.MediaList.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
