589c0c01b212f5a55f8d08b631792b5efc16a0d3
dennis1984 <year2005706@163.com>
2017-11-28 21:59:39 +0800

    报告文件下载：开发

--- reports/forms.py
+++ reports/forms.py
@@ -5,3 +5,7 @@ from horizon import forms
 class ReportListForm(forms.Form):
     page_index = forms.IntegerField(min_value=1, required=False)
     page_size = forms.IntegerField(min_value=1, required=False)
+
+
+class ReportFileDownloadForm(forms.Form):
+    media_id = forms.IntegerField(min_value=1)
--- reports/views.py
+++ reports/views.py
@@ -1,11 +1,13 @@
 # -*- coding: utf8 -*-
+from django.http import StreamingHttpResponse
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
 from reports.serializers import (ReportListSerializer,)
 from reports.permissions import IsOwnerOrReadOnly
 from reports.models import (Report, ReportDownloadRecord)
-from reports.forms import (ReportListForm,)
+from reports.forms import (ReportListForm,
+                           ReportFileDownloadForm)
 
 import json
 
@@ -37,3 +39,37 @@ class ReportList(generics.GenericAPIView):
             return Response({'Detail': data_list.args}, status=status.HTTP_400_BAD_REQUEST)
         return Response(data_list, status=status.HTTP_200_OK)
 
+
+class ReportFileDownload(generics.GenericAPIView):
+    """
+    媒体资源报告文件下载
+    """
+    permission_classes = (IsOwnerOrReadOnly,)
+
+    def get_report_object(self, media_id):
+        return Report.get_object(media_id=media_id)
+
+    def download_file(self, file_name, buffer_size=512):
+        with open(file_name) as fp:
+            while True:
+                text = fp.read(buffer_size)
+                if text:
+                    yield text
+                else:
+                    break
+
+    def post(self, request, *args, **kwargs):
+        form = ReportFileDownloadForm(request.data)
+        if not form.is_valid():
+            return Response({'Detail': form.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+        cld = form.cleaned_data
+        instance = self.get_report_object(cld['media_id'])
+        if isinstance(instance, Exception):
+            return Response({'Detail': instance.args}, status=status.HTTP_400_BAD_REQUEST)
+
+        file_name = instance.report_file.name
+        response = StreamingHttpResponse(self.download_file(file_name))
+        response['Content-Type'] = 'application/octet-stream'
+        response['Content-Disposition'] = 'attachment;filename=%s' % file_name
+        return response
